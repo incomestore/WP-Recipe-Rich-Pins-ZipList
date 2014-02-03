cc019d1605dd54b7b629530ea1e5fed562868887
Nick Young <mycorpweb@gmail.com>
2014-02-02 22:57:33 -0800

    Added function to output og:site_name

--- recipe-rich-pins-for-ziplist/views/public.php
+++ recipe-rich-pins-for-ziplist/views/public.php
@@ -11,3 +11,17 @@
  * @copyright 2014 Phil Derksen
  */
 
+/*
+ * Add og:site_name to the header of the site
+ * 
+ * @since 1.0.0
+ */
+function rrpzl_add_og_sitename() {
+	
+	global $rrpzl_options;
+	
+	if( ! empty( $rrpzl_options['og_site_name'] ) ) {
+		echo '<meta property="og:site_name" content="' . esc_attr( $rrpzl_options['og_site_name'] ) . '" />' . "\n";
+	}
+}
+add_action( 'wp_head', 'rrpzl_add_og_site_name' );
