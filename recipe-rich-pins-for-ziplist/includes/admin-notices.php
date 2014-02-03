15b9a389ba8a44d47fe739aa76b2cd238a30ce06
Nick Young <mycorpweb@gmail.com>
2014-02-02 22:47:39 -0800

    Added admin save settings message

--- recipe-rich-pins-for-ziplist/includes/admin-notices.php
+++ recipe-rich-pins-for-ziplist/includes/admin-notices.php
@@ -11,3 +11,17 @@
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) )
 	exit;
+
+/*
+ * Display save settings message in the admin
+ * 
+ * @since 1.0.0
+ */
+function rrpzl_register_admin_notices() { 
+	
+	if ( ( isset( $_GET['page'] ) && 'recipe-rich-pins-for-ziplist' == $_GET['page'] ) && ( isset( $_GET['settings-updated'] ) && 'true' == $_GET['settings-updated'] ) ) {
+		add_settings_error( 'rrpzl-notices', 'rrpzl-general-updated', __( 'Settings updated.', 'rrpzl' ), 'updated' );
+	}
+
+}
+add_action( 'admin_notices', 'rrpzl_register_admin_notices' );
