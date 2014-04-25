3a78dc131b05f4812050b24b191636c8bdc6860e
Nick Young <mycorpweb@gmail.com>
2014-04-25 14:20:49 -0600

    Updated version number in main class

--- recipe-rich-pins-ziplist/class-recipe-rich-pins-ziplist.php
+++ recipe-rich-pins-ziplist/class-recipe-rich-pins-ziplist.php
@@ -27,7 +27,7 @@ class Recipe_Rich_Pins_ZipList {
 	 * and README.txt changelog
 	 **************************************/
 
-	const VERSION = '1.0.0';
+	const VERSION = '1.0.1';
 
 	/**
 	 * Unique identifier for your plugin.
